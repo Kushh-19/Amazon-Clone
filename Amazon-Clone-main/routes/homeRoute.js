db0c15f505dcc1c567292f316ec60e58f491db7a
VaTSal1245 <shahvatsal523@gmail.com>
2025-04-17 21:43:49 +0530

    Included routes whenever necessary

--- Amazon-Clone-main/routes/homeRoute.js
+++ Amazon-Clone-main/routes/homeRoute.js
@@ -3,7 +3,38 @@ const Router = express.Router();
 
 
 Router.get('/',(req,res) => {
-    res.send('index');
+    res.render('index.ejs');
 });
 
+Router.get('/location', (req,res)=> {
+    res.send("location")
+});
+
+Router.get('/return-order', (req,res) => {
+    res.send("Return & Orders");
+});
+
+Router.get('/cart', (req,res) => {
+    res.send("Cart");
+});
+
+Router.get('/todays-deals', (req,res) => {
+    res.send("Todays Deals");
+});
+
+Router.get('/customer-service', (req,res) => {
+    res.send("Customer Service");
+});
+
+Router.get('/registery', (req,res) => {
+    res.send("Registery");
+});
+
+Router.get('/gift-cards', (req,res) => {
+    res.send("Gift Cards");
+});
+
+Router.get('/sell', (req,res) => {
+    res.send("Sell");
+});
 module.exports = Router
